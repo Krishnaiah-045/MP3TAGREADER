9eb51df729e43bdb17fcc52f8863690a9d7f8b30
Krishnaiah-045 <krishnaiahmandala8@gmail.com>
2025-09-18 18:57:25 +0530

    mp3.c

--- mp3.c
+++ mp3.c
@@ -3,12 +3,7 @@
 #include <string.h>
 #include <unistd.h>  
 #include "header.h"
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define CYAN    "\033[36m"
-#define BOLD    "\033[1m"
+
 
 void showLoading(const char *message)
 {
